a2e1b54bd9f153f7e553b725878552f8d5ffa82b
The Lambda Calculus <code@thelambdacalculus.net>
2012-01-12 23:07:20 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var static = require('node-static');
+var file = new static.Server('./static');
+
+require('http').createServer(function(request, response) {
+    request.addListener('end', function() {
+        file.serve(request, response);
+    });
+}).listen(8080);
