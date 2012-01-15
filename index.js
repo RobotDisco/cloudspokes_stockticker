730379f0fdf9517e0e1238955321c40448546d03
The Lambda Calculus <code@thelambdacalculus.net>
2012-01-15 14:41:04 -0500

    Converted project to use Express and Jade

--- index.js
+++
@@ -1,8 +0,0 @@
-var static = require('node-static');
-var file = new static.Server('./static');
-
-require('http').createServer(function(request, response) {
-    request.addListener('end', function() {
-        file.serve(request, response);
-    });
-}).listen(8080);
