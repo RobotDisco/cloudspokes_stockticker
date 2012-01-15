84ebae97bfb5271ae820ce1b36442419497dd690
The Lambda Calculus <code@thelambdacalculus.net>
2012-01-15 16:56:11 -0500

    Added retrieval of CSV file for a fixed set of stocks, no rendering yet.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+exports.index = function(req, res) {
+    var options = {
+        hostname: 'download.finance.yahoo.com',
+        path: '/d/quotes.csv?s=GOOG+NFLX+BCSI+EA+ZNGA&f=sl1'
+    };
+
+    require('http').get(options, function(response) {
+        res.render('index', { title: 'Express' });
+    });
 };
