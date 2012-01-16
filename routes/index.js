4a91724cde1ee9eb47796ed355f38420ebeef6c6
The Lambda Calculus <code@thelambdacalculus.net>
2012-01-15 20:54:44 -0500

    On page load, we grab values from Yahoo finance and display them in the ticker.

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,19 @@ exports.index = function(req, res) {
         });
 
         response.on('end', function() {
-            res.render('index', {
-                layout: false
+            var csv = require('csv');
+            var stock_data = {};
+            var payload = csv().from(body, { columns: false });
+
+            payload.on('data', function(data, index) {
+                stock_data[data[0]] = data[1];
+            });
+
+            payload.on('end', function() {
+                res.render('index', {
+                    layout: false,
+                    payload: stock_data
+                });
             });
         });
     });
